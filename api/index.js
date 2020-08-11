5aa6911772626693f5400413e2c623c5cf662b73
Axel Espinosa <axeldavid45@gmail.com>
2020-08-11 13:49:59 -0500

    Initial structure

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const config = require('../config')
+const user = require('./components/user/network');
+// Router
+app.use('/api/user', user)
+app.listen(config.api.port, () => console.log('Listening on port', config.api.port))
+
