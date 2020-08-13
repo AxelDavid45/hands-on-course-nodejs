a988c52323b576acaf7f9a73179e00c4172fe362
Axel Espinosa <axeldavid45@gmail.com>
2020-08-13 12:49:31 -0500

    Added eslint

--- network/response.js
+++ network/response.js
@@ -1,15 +1,20 @@
 'use strict'
 exports.success = function (req, res, message = 'ok', status = 200) {
-	res.status(status).send({
-		status: status,
-		body: message
-	});
+  res.status(status).send({
+    status: status,
+    body: message
+  })
 }
 
-exports.error = function (req, res, message = 'Internal Server Error', status = 500) {
-	res.status(status).send({
-		error: true,
-		status: status,
-		body: message
-	});
+exports.error = function (
+  req,
+  res,
+  message = 'Internal Server Error',
+  status = 500
+) {
+  res.status(status).send({
+    error: true,
+    status: status,
+    body: message
+  })
 }
